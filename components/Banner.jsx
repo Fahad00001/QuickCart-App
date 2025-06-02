55f15bc24e38495c3bde2b8412505533e7f2e37a
Fahad <145900785+Fahad00001@users.noreply.github.com>
2025-06-02 22:35:23 +0530

    third commit

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
       />
       <div className="flex flex-col items-center justify-center text-center space-y-2 px-4 md:px-0">
         <h2 className="text-2xl md:text-3xl font-semibold max-w-[290px]">
-          Level Up Your Gaming Experience
+          Not Level Up Your Gaming Experience
         </h2>
         <p className="max-w-[343px] font-medium text-gray-800/60">
           From immersive sound to precise controls—everything you need to win
