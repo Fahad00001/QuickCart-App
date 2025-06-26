1c699ec0e7dcd10d86badb9ced69701dd567a2f5
Fahad <145900785+Fahad00001@users.noreply.github.com>
2025-06-26 21:55:44 +0530

    save

--- config/inngest.js
+++ config/inngest.js
@@ -2,7 +2,8 @@ import { Inngest } from "inngest";
 import dbConnect from "./db";
 
 import Order from "@/models/Order";
-import User from "@/models/User";
+// import User from "@/models/User";
+import User from "../models/User";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
